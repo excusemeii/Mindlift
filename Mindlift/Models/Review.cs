7ef87c59c764cd79eb2ab83ed7e4d63b70bda958
joshikn <joshikn@LCB-09>
2024-11-05 16:54:58 -0500

    Adding Model classes as required for Mindlift

---
+++ Mindlift/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Mindlift.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        [Required(ErrorMessage = "Rating is required")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+        public int Rating { get; set; }
+
+        [Required(ErrorMessage = "Comment is required")]
+        [StringLength(500, ErrorMessage = "Comment cannot be more than 500 characters")]
+        public string Comment { get; set; }
+
+        [DisplayName("Review Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReviewDate { get; set; }
+
+        // many-to-many relationship with Book table
+        public List<Book>? Book { get; set; }
+    }
+}
