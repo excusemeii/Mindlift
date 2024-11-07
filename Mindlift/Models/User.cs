8ab2c388a7065321bdf2b1f63c544813c32998ee
Deepika Srivastava <srivasdv@mail.uc.edu>
2024-11-06 22:02:27 -0500

    Creating and modification of authentication cascade pages__00

---
+++ Mindlift/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Mindlift.Models
+{
+    // User class representing a forum user
+    public class User
+    {
+        public int UserID { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public DateTime? JoinDate { get; set; }
+
+    }
+}
