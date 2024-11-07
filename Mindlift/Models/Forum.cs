b8e2ac4c6151db47a4836eefc6797c00c87cca19
Deepika Srivastava <srivasdv@mail.uc.edu>
2024-11-06 22:14:09 -0500

    Created and ModifieForum UI and List pages

--- Mindlift/Models/Forum.cs
+++ Mindlift/Models/Forum.cs
@@ -12,4 +12,4 @@ namespace Mindlift.Models
 
         }
           
-    }
+}
---
+++ Mindlift/Pages/Forums/ForumList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mindlift.Pages.Forums
+{
+    public class ForumListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
