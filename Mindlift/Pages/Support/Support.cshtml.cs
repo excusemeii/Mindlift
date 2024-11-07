cce9c4f10f082c3cc8744ce100d923fa944ae4f4
yashbheke2000 <yash.bheke2000@gmail.com>
2024-11-07 10:59:31 -0500

    added support page

---
+++ Mindlift/Pages/Support/Support.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mindlift.Pages.Support
+{
+    public class SupportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
