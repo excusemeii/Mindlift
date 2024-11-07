d96bd7b0432bea25884fcc1ce7be12f1677ac5e5
AbhiBadola <abhishekbadola11@gmail.com>
2024-11-06 20:44:23 -0500

    Created User and WishList models with mandatory field validations.

---
+++ Mindlift/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mindlift.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, ErrorMessage = "Username can't be longer than 50 characters")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [StringLength(100, ErrorMessage = "Email can't be longer than 100 characters")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(100, ErrorMessage = "Full name can't be longer than 100 characters")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string PasswordHash { get; set; } 
+
+      
+        public ICollection<Wishlist> Wishlists { get; set; }
+    }
+}
